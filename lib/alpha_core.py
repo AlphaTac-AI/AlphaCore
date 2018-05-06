dda45e77aecda7e297e260edd940d2fe18f161df
Vector <Jiraiya812@YIs-MacBook-Pro.local>
2018-05-06 10:32:15 -0400

    refactor project structure

--- lib/alpha_core.py
+++ lib/alpha_core.py
@@ -3,14 +3,108 @@ import gc
 import xgboost as xgb
 from xgboost import XGBClassifier, plot_importance
 from sklearn import metrics
+from lib.utils  import *
+from lib.feature_lib import *
 
-def build_features(df, feature_pipeline):
-    feature_set=set()
-    for fun in feature_pipeline:
-        df, feature_set=fun(df, feature_set)
-    df = df[list(feature_set)]
-    gc.collect()
-    return df, feature_set
+
+
+class Solution:
+    train_df = None
+    label_df = None
+    test_df = None
+
+    f_ppl = []
+    train_f_set = None
+    test_f_set = None
+
+    model = None
+    train_result = None
+    test_result = None
+
+    log = None
+    method = 'xgb'
+    input_path = '../input/'
+    output_path = '../output/'
+    data_set = None
+    transductive =False
+    para_tune_fcg = None
+
+    def __init__(self):
+        pass
+
+    def load_dataset(self):
+        tic = report("Load Dataset Start")
+        if self.transductive:
+            self.data_set.load_train(self)
+            self.data_set.load_test(self)
+        else:
+            self.data_set.load_test(self)
+        report("Load Dataset Done", tic)
+
+    def build_features(self):
+        tic = report("Build Features Start")
+        if self.transductive:
+            merge = pd.concat([self.train_df, self.test_df])
+
+            merge, f_set = build_features(merge, self.f_ppl)
+
+            self.train_f_set,self.test_f_set = f_set,f_set
+
+            self.train_df, self.test_df = merge[:self.train_df.shape[0]],merge[self.train_df.shape[0]:].reset_index(drop=True)
+        else:
+            self.train_df, self.train_f_set = build_features(self.train_df, self.f_ppl)
+
+            self.test_df, self.test_f_set = build_features(self.test_df, self.f_ppl)
+
+            if self.train_f_set !=self.test_f_set:
+
+                print("Warning training featue set is different with testing feature set")
+
+        print("Feature Selected:\t{0}".format(','.join(self.train_f_set)))
+        report("Build Features Done", tic)
+        print(self.train_df.head(5))
+        print(self.test_df.head(5))
+
+    def init_model(self):
+        if self.method == 'xgb':
+            xgb_init(self)
+        elif self.method == 'lgbm':
+            pass
+
+    def para_tune(self):
+        tic = report("Parameter Tuning Start")
+        if self.method == 'xgb':
+            xgb_pt(self)
+        elif self.method == 'lgbm':
+            pass
+        report("Parameter Tuning Done", tic)
+
+    def train(self):
+        tic = report("Model training Start")
+        if self.method == 'xgb':
+            xgb_train(self)
+        elif self.method == 'lgbm':
+            pass
+        report("Model training Done", tic)
+
+    def test(self):
+        tic = report("Test Start")
+        if self.method == 'xgb':
+            xgb_test(self)
+        elif self.method == 'lgbm':
+            pass
+        report("Test Done", tic)
+
+    def save_test(self):
+        check_dir(self.output_path)
+        self.test_result.to_csv(self.output_path + self.result_filename, float_format='%.8f', index=False)
+
+
+
+
+
+
+
 
 def xgb_init(s):
     s.model =  XGBClassifier(learning_rate=0.3,
---
+++ lib/feature_lib.py
@@ -0,0 +1,10 @@
+import gc
+
+def build_features(df, feature_pipeline):
+    feature_set=set()
+    for fun in feature_pipeline:
+        df, feature_set=fun(df, feature_set)
+    df = df[list(feature_set)]
+    gc.collect()
+    return df, feature_set
+
